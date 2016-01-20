7be21d3a016df8a790593a0c9bb3f9c1d953a791
eirirlar <eirirlar@gmail.com>
2016-01-20 14:44:12 +0100

    log scan

--- src/main/scala/com/kodeworks/doffapp/ctx/Cfg.scala
+++ src/main/scala/com/kodeworks/doffapp/ctx/Cfg.scala
@@ -6,7 +6,10 @@ import scala.util.Try
 
 trait Cfg {
   val baseConfig = ConfigFactory.load
+  val envConfig = ConfigFactory.parseResources(Try(baseConfig.getString("env")).getOrElse("") + ".conf")
+  val envConfigFull = envConfig.withFallback(baseConfig)
   val userConfig =
-    ConfigFactory.parseResources(Try(baseConfig.getString("user.name")).getOrElse("") + ".conf")
-  val config = userConfig.withFallback(baseConfig)
+    ConfigFactory.parseResources(Try(envConfigFull.getString("user.name")).getOrElse("") + ".conf")
+  val userConfigFull = userConfig.withFallback(envConfigFull)
+  val config = userConfigFull
 }
